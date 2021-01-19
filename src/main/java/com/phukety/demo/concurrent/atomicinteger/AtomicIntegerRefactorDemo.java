086808ed182d7f299df9e2964bd6d3c1fe7e2f97
Phukety <1252615137@qq.com>
2021-01-20 00:15:18 +0800

    AtomicIntegerRefactorDemo

--- src/main/java/com/phukety/demo/concurrent/atomicinteger/AtomicIntegerRefactorDemo.java
+++ src/main/java/com/phukety/demo/concurrent/atomicinteger/AtomicIntegerRefactorDemo.java
@@ -3,12 +3,12 @@ package com.phukety.demo.concurrent.atomicinteger;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class AtomicIntegerRefactorDemo implements Runnable {
-    private static AtomicInteger k = new AtomicInteger(0);
+    private static final AtomicInteger k = new AtomicInteger(0);
 
     @Override
     public void run() {
         for (int i = 0; i < 10000; i++) {
-            k.getAndIncrement();
+            k.incrementAndGet();
         }
     }
 
