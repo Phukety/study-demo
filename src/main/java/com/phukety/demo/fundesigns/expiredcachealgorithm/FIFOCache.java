8d1e0d611a0822b35eaa4b08ee1cb3c0dcd7bd35
Phukety <1252615137@qq.com>
2022-01-16 20:20:44 +0800

    add - FIFO过期缓存算法实现

--- src/main/java/com/phukety/demo/fundesigns/expiredcachealgorithm/FIFOCache.java
+++ src/main/java/com/phukety/demo/fundesigns/expiredcachealgorithm/FIFOCache.java
@@ -1,13 +1,116 @@
 package com.phukety.demo.fundesigns.expiredcachealgorithm;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
 /**
  * 过期缓存算法 - FIFO(First Input First Output)
  * 先进先出，如果缓存容量满，则优先移出最早加入缓存的数据；其内部可以使用队列实现。
- *
+ * <p>
  * 设计思路====>
- * 1. LinkedHashMap保存缓存数据，保证时间顺序
+ * 1. LinkedHashMap保存缓存数据，保证时间顺序。需要频繁查找节点，且需要断链和接链
  * 2. 额外的map用来保存key的过期特性，比如TreeMap，将“剩余存活时间”作为key，利用Treemap的排序特性
- *
+ * <p>
+ * 淘汰机制（添加时，若缓存容量已达上限）====>
+ * 1. 先移除缓存队列中已经满足过期的键
+ * 2. 若还不够，则再根据想先进先出的原则，移除最早加入缓存的数据
  */
-public class FIFOCache {
+public class FIFOCache<T> {
+    // 缓存容量
+    private final int capacity;
+    // 缓存载体<key,value>
+    private final Map<String, T> cache = new LinkedHashMap<>();
+    // 有过期时间的key载体<key,expireTime>
+    private final Map<String, Long> expire = new HashMap<>();
+
+    public FIFOCache() {
+        this(0);
+    }
+
+    public FIFOCache(int capacity) {
+        this.capacity = capacity == 0 ? 16 : capacity;
+    }
+
+
+    /**
+     * 返回缓存内容
+     *
+     * @param key 键
+     * @return null:当key已经过期
+     */
+    T get(String key) {
+        if (key == null) {
+            return null;
+        }
+        if (expire.containsKey(key) && expire.get(key) <= System.currentTimeMillis()) {
+            expire.remove(key);
+            return null;
+        }
+        return cache.get(key);
+    }
+
+    /**
+     * 不带过期时间的缓存
+     *
+     * @param key   键
+     * @param value 值
+     * @return true:成功, false:失败
+     */
+    boolean put(String key, T value) {
+        return put(key, value, -1, TimeUnit.MILLISECONDS);
+    }
+
+    /**
+     * 添加带有过期时间的缓存
+     *
+     * @param key         键
+     * @param value       值
+     * @param expiredTime 过期时间
+     * @param unit        时间单位
+     * @return true:成功, false:失败
+     */
+    boolean put(String key, T value, long expiredTime, TimeUnit unit) {
+        if (cache.containsKey(key)) {
+            return false;
+        }
+        // 若容量上限，先移除过期key
+        if (cache.size() >= capacity) {
+            for (Map.Entry<String, Long> entry : expire.entrySet()) {
+                // 未过期则跳过该键
+                if (entry.getValue() > System.currentTimeMillis()) {
+                    continue;
+                }
+                // 移除过期key
+                cache.remove(entry.getKey());
+            }
+        }
+        // 仍然容量上限，则移除最早加入缓存的数据
+        if (cache.size() >= capacity) {
+            Iterator<Map.Entry<String, T>> iterator = cache.entrySet().iterator();
+            while (iterator.hasNext() && cache.size() >= capacity) {
+                // 移除过期key
+                expire.remove(iterator.next().getKey());
+                // 移除元素
+                iterator.remove();
+            }
+        }
+        // 此时容量一定是足够的,再进行添加
+        if (expiredTime >= 0) {
+            expire.put(key, System.currentTimeMillis() + unit.toMillis(expiredTime));
+        }
+        cache.put(key, value);
+        return true;
+    }
+
+    /**
+     * 当前缓存元素总量
+     *
+     * @return 缓存元素总数
+     */
+    public int size() {
+        return cache.size();
+    }
 }
---
+++ src/test/java/com/phukety/demo/fundesigns/expiredcachealgorithm/FIFOCacheTest.java
@@ -0,0 +1,115 @@
+package com.phukety.demo.fundesigns.expiredcachealgorithm;
+
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.concurrent.*;
+
+import static org.junit.Assert.*;
+
+public class FIFOCacheTest {
+    @Test
+    public void testFIFOCache() {
+        FIFOCache<String> cache = new FIFOCache<>(5);
+
+        cache.put("1", "1");
+        cache.put("2", "2");
+        assertTrue(cache.put("3", "3"));
+
+        assertEquals(cache.size(), 3);
+        assertEquals(cache.get("1"), "1");
+
+        cache.put("4", "4");
+        assertTrue(cache.put("5", "5"));
+
+        assertEquals(cache.size(), 5);
+        assertEquals(cache.get("5"), "5");
+
+        assertTrue(cache.put("6", "6"));
+
+        assertEquals(cache.size(), 5);
+        assertEquals(cache.get("5"), "5");
+        assertEquals(cache.get("6"), "6");
+        assertNull(cache.get("1"));
+
+        assertFalse(cache.put("6", "7"));
+        assertEquals(cache.size(), 5);
+        assertEquals(cache.get("6"), "6");
+        assertNull(cache.get("1"));
+    }
+
+    @Test
+    public void testExpiredFIFOCache() throws InterruptedException {
+        FIFOCache<String> cache = new FIFOCache<>(5);
+
+        cache.put("1", "1", 1L, TimeUnit.SECONDS);
+        cache.put("2", "2", 2L, TimeUnit.SECONDS);
+        cache.put("3", "3", 2L, TimeUnit.SECONDS);
+        cache.put("4", "4");
+        cache.put("5", "5");
+
+        assertEquals(cache.size(), 5);
+        assertEquals(cache.get("1"), "1");
+        assertEquals(cache.get("5"), "5");
+
+        Thread.sleep(1000);
+        assertTrue(cache.put("6", "6", 2L, TimeUnit.SECONDS));
+        assertEquals(cache.size(), 5);
+        assertNull(cache.get("1"));
+        assertEquals(cache.get("2"), "2");
+
+        Thread.sleep(1000);
+
+        assertTrue(cache.put("7", "7"));
+        assertEquals(cache.size(), 4);
+        assertTrue(cache.put("8", "8"));
+        assertEquals(cache.size(), 5);
+        assertNull(cache.get("1"));
+        assertNull(cache.get("2"));
+        assertNull(cache.get("3"));
+        assertEquals(cache.get("4"), "4");
+
+        Thread.sleep(1000);
+        assertTrue(cache.put("9", "9"));
+        assertEquals(cache.size(), 5);
+        assertNull(cache.get("6"));
+    }
+
+    @Test
+    public void testBoundary() {
+        FIFOCache<String> cache = new FIFOCache<>();
+        int i = 0;
+        int capacity = cache.size();
+        while (cache.put(String.valueOf(i), String.valueOf(i)) && capacity != cache.size()) {
+            i++;
+            capacity = cache.size();
+        }
+        assertEquals(cache.size(), 16);
+
+        assertNull(cache.get(null));
+        assertNull(cache.get(""));
+    }
+
+    @Test
+    public void testGenerics() throws InterruptedException, ExecutionException {
+        FIFOCache<Integer> integerCache = new FIFOCache<>();
+        integerCache.put("1", 1);
+        integerCache.put("2", 2);
+        integerCache.put("3", 3);
+        assertEquals(integerCache.size(), 3);
+        assertEquals(integerCache.get("3").intValue(), 3);
+
+        FIFOCache<ExecutorService> executorCache = new FIFOCache<>();
+        executorCache.put("cached", Executors.newCachedThreadPool());
+        executorCache.put("fixed", Executors.newFixedThreadPool(2));
+        executorCache.put("single", Executors.newSingleThreadExecutor());
+        assertEquals(executorCache.size(), 3);
+
+        ExecutorService executorService = executorCache.get("single");
+        Boolean result = executorService.invokeAny(new ArrayList<Callable<Boolean>>() {{
+            add(Executors.callable(() -> System.out.println("task is running"), true));
+        }});
+        assertEquals(result, true);
+    }
+}
